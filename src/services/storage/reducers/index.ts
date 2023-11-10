128c3f6ba99f0a321b18402155d2ac103797e850
Yakup Afsin <yakupafsin422@gmail.com>
2023-11-10 15:51:32 +0000

    Update index.ts

--- src/services/storage/reducers/index.ts
+++ src/services/storage/reducers/index.ts
@@ -1,9 +1,20 @@
-import { combineReducers } from "redux";
+// reducers/index.ts
+import { combineReducers, Reducer } from "redux";
 
-import postReducer from "./posts";
+import postsReducer, { PostsState } from "./posts";
 
-const rootReducer = combineReducers({
-  posts: postReducer,
+// Define the type for the combined state
+export interface RootState {
+  posts: PostsState;
+  // Add other slices of state as needed
+}
+
+// Define the type for the combined reducer
+export type CombinedReducer = Reducer<RootState>;
+
+const rootReducer: CombinedReducer = combineReducers<RootState>({
+  posts: postsReducer,
+  // Add other slices of state as needed
 });
 
 export default rootReducer;
