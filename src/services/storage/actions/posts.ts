298a07149c10aac9794d677e649f1a0f19a8b70e
Yakup Afsin <yakupafsin422@gmail.com>
2023-11-10 15:51:39 +0000

    Update posts.ts

--- src/services/storage/actions/posts.ts
+++ src/services/storage/actions/posts.ts
@@ -1,17 +1,42 @@
 // src/actions/posts.ts
+import { Dispatch } from "redux";
+
 import * as api from "../../api/wordpressApi";
 
+// Action types
 export const FETCH_POSTS_REQUEST = "FETCH_POSTS_REQUEST";
 export const FETCH_POSTS_SUCCESS = "FETCH_POSTS_SUCCESS";
 export const FETCH_POSTS_FAILURE = "FETCH_POSTS_FAILURE";
 
-export const fetchPosts = () => async (dispatch) => {
-  dispatch({ type: FETCH_POSTS_REQUEST });
+// Action creators
+interface FetchPostsRequestAction {
+  type: typeof FETCH_POSTS_REQUEST;
+}
 
-  try {
-    const posts = await api.get("/posts");
-    dispatch({ type: FETCH_POSTS_SUCCESS, payload: posts });
-  } catch (error) {
-    dispatch({ type: FETCH_POSTS_FAILURE, payload: error.message });
-  }
-};
+interface FetchPostsSuccessAction {
+  type: typeof FETCH_POSTS_SUCCESS;
+  payload: any[]; // Update the type of payload as needed
+}
+
+interface FetchPostsFailureAction {
+  type: typeof FETCH_POSTS_FAILURE;
+  payload: string;
+}
+
+export type PostsActionTypes =
+  | FetchPostsRequestAction
+  | FetchPostsSuccessAction
+  | FetchPostsFailureAction;
+
+// Async action creator
+export const fetchPosts =
+  () => async (dispatch: Dispatch<PostsActionTypes>) => {
+    dispatch({ type: FETCH_POSTS_REQUEST });
+
+    try {
+      const posts = await api.get("/posts");
+      dispatch({ type: FETCH_POSTS_SUCCESS, payload: [posts] });
+    } catch (error) {
+      dispatch({ type: FETCH_POSTS_FAILURE, payload: error.message });
+    }
+  };
