b5b2bf1410310a232c79656be62ef78a035eeffc
Yakup Afsin <yakupafsin422@gmail.com>
2023-11-11 05:08:49 +0000

    Update BookmarksPage.tsx

--- src/screens/BookmarksPage.tsx
+++ src/screens/BookmarksPage.tsx
@@ -1,16 +1,19 @@
+import { usePosts } from "@storage/hooks/usePosts"; // Adjust the import path based on your project structure
+import React, { useEffect } from "react";
 import { View, Text } from "react-native";
-import { connect } from "react-redux";
 
-const BookMarksPage = ({ sampleData }) => {
+const BookmarksPage: React.FC = () => {
+  const { posts, loading, error } = usePosts();
+  console.log(posts);
+  useEffect(() => {
+    // Do something with posts, loading, error
+  }, [posts, loading, error]);
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Bookmarks!</Text>
+      <Text>BookMarks!</Text>
     </View>
   );
 };
 
-const mapStateToProps = (state) => ({
-  sampleData: state.sampleReducer.sampleData,
-});
-
-export default connect(mapStateToProps)(BookMarksPage);
+export default BookmarksPage;
