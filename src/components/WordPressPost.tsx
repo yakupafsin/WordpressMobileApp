30be52e57915a938384a580211a11f30abf85241
Yakup Afsin <yakupafsin422@gmail.com>
2023-11-11 16:20:58 +0000

    Create WordPressPost.tsx

---
+++ src/components/WordPressPost.tsx
@@ -0,0 +1,28 @@
+import { WordPressPostProps } from "@components/types";
+import { htmlTagStyles, postStyles } from "@utils/styles";
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+import HTMLRenderer from "./HtmlRenderer";
+const WordPressPost: React.FC<WordPressPostProps> = ({
+  title,
+  imageUrl,
+  excerpt,
+}) => {
+  const defaultTextProps = {
+    numberOfLines: 4,
+  };
+  return (
+    <View style={postStyles.container}>
+      <Image source={{ uri: imageUrl }} style={postStyles.image} />
+      <Text style={postStyles.title}>{title}</Text>
+      <HTMLRenderer
+        tagsStyles={htmlTagStyles.tagStyles}
+        htmlContent={excerpt}
+        defaultTextProps={defaultTextProps}
+      />
+    </View>
+  );
+};
+
+export default WordPressPost;
