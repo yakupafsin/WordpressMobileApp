edbe10420a26eb27ad5e9946fb25fdf33caf553f
Yakup Afsin <yakupafsin422@gmail.com>
2023-11-11 16:21:02 +0000

    Update ExplorePage.tsx

--- src/screens/ExplorePage.tsx
+++ src/screens/ExplorePage.tsx
@@ -1,17 +1,32 @@
+import WordPressPost from "@components/WordPressPost";
 import { usePosts } from "@storage/hooks/usePosts"; // Adjust the import path based on your project structure
+import { decodeText } from "@utils/helper";
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, ScrollView, RefreshControl } from "react-native";
 
 const ExplorePage: React.FC = () => {
-  const { posts, loading, error } = usePosts();
-  console.log(posts);
+  const { posts, loading, error, refetchPosts } = usePosts();
+
   useEffect(() => {
     // Do something with posts, loading, error
   }, [posts, loading, error]);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={refetchPosts} />
+        }
+      >
+        {posts.map((item, index) => (
+          <WordPressPost
+            imageUrl={item.jetpack_featured_media_url}
+            title={decodeText(item.title.rendered)}
+            excerpt={decodeText(item.excerpt.rendered)}
+            key={index}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 };
